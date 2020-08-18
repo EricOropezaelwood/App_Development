c9439bdc5589d2a92ea138d42958138d45246b00
EricOropezaelwood <eric.oropezaelwood@gmail.com>
2020-08-17 20:37:23 -0500

    added get http request, pretty cool to see how it works with dart

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,9 +22,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
     print(location.longitude);
   }
+  
+  void getData() async {
+    Response response = await get('https://samples.openweathermap.org/data/2.5/'
+        'weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
