754c7f75e12bd7c918e6cd4dea9d7491f0286c62
EricOropezaelwood <eric.oropezaelwood@gmail.com>
2020-08-18 20:51:43 -0500

    split the api json return into variables. learned how to grab from this sample API

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -28,10 +29,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
     if (response.statusCode == 200) {
       String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+
     } else {
       print(response.statusCode);
     }
-    print(response.body);
+//    print(response.body);
   }
 
   @override
